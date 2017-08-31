f6b0e7534313cb64a6c0f5e357aa82c25cde9a9c
donkey_hotei <donkey-hotei@cryptolab.net>
2017-08-30 20:33:26 -0700

    add first asset pack

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,13 @@
 package;
 
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
     override public function create():Void
     {
+        add(new FlxText(10, 10, 100, "Welcome to the Galaxy."));
         super.create();
     }
 
